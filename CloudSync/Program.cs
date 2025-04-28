772cc301bba09d47e96a2464ec3629f9160b64b8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-28 19:30:15 -0400

    add Swagger for API testing

--- CloudSync/Program.cs
+++ CloudSync/Program.cs
@@ -1,17 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "PROJXON HRIS API",
+        Version = "v1",
+        Description = "Web server for the management of PROJXON HRIS data.",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "PROJXON",
+            Email = "it@projxon.com",
+            Url = new Uri("https://projxon.com")
+        },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
