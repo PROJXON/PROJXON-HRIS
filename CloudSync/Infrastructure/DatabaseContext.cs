e05859a1dacbab3a06619aed4176d38c9de64a92
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-29 22:17:08 -0400

    add OnModelCreating for setting primary keys

--- CloudSync/Infrastructure/DatabaseContext.cs
+++ CloudSync/Infrastructure/DatabaseContext.cs
@@ -10,5 +10,17 @@ public class DatabaseContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
-    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> Roles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasKey(u => u.Id);
+        
+        modelBuilder.Entity<UserRole>()
+            .HasKey(u => u.Id);
+        
+        modelBuilder.Entity<Permission>()
+            .HasKey(u => u.Id);
+    }
 }
