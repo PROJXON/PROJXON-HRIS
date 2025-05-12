6a6f3f9b5a5bebc0f85019e85ffcdc9ff346a68c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-12 18:06:53 -0400

    add InvitedUser model for managing invited users in user management

---
+++ CloudSync/Modules/UserManagement/Models/InvitedUser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudSync.Modules.UserManagement.Models;
+
+public class InvitedUser
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [EmailAddress]
+    [StringLength(255)]
+    public string? Email { get; set; }
+    
+    [StringLength(255)]
+    public User? InvitedBy { get; set; }
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime CreateDateTime { get; set; }
+}
