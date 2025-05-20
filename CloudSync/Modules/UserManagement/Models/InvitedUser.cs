5e8d169e1120d55b6bbd4219ae476698e14dc301
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-20 17:43:04 -0400

    update Id and Status so that they're required fields, add InvitedBy as required and as foreign key, and update InvitedUser model to use InvitedUserStatus enum

--- CloudSync/Modules/UserManagement/Models/InvitedUser.cs
+++ CloudSync/Modules/UserManagement/Models/InvitedUser.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Shared.Enums.UserManagement;
 
 namespace CloudSync.Modules.UserManagement.Models;
 
 public class InvitedUser
 {
+    [Required]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
@@ -12,12 +14,16 @@ public class InvitedUser
     [StringLength(255)]
     public string? Email { get; set; }
     
-    [StringLength(255)]
+    [Required]
+    public int InvitedById { get; set; }
+    
+    [ForeignKey("InvitedById")]
     public User? InvitedBy { get; set; }
     
     [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTime CreateDateTime { get; set; }
     
+    [Required]
     [StringLength(20)]
-    public required string Status { get; set; }
+    public required InvitedUserStatus Status { get; set; }
 }
