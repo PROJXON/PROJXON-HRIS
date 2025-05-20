57c0df9bc8c471ab4364ac1c34d1f5c6f9ae597f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-20 17:51:49 -0400

    add IInvitedUserRepository interface for managing invited users

---
+++ CloudSync/Modules/UserManagement/Repositories/IInvitedUserRespository.cs
@@ -0,0 +1,12 @@
+﻿using CloudSync.Modules.UserManagement.Models;
+
+namespace CloudSync.Modules.UserManagement.Repositories;
+
+public interface IInvitedUserRespository
+{
+    IEnumerable<InvitedUser> GetAll();
+    InvitedUser GetById(int id);
+    void Add(InvitedUser invitedUser);
+    void Update(InvitedUser invitedUser);
+    void Delete(int id);
+}
