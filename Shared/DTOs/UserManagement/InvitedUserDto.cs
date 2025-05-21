ce0edebe704c0772750f58dda68071fd6b96eda1
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-21 14:21:45 -0400

    refactor InvitedUserDto to use InvitedUserStatus enum for status property

--- Shared/DTOs/UserManagement/InvitedUserDto.cs
+++ Shared/DTOs/UserManagement/InvitedUserDto.cs
@@ -1,8 +1,10 @@
-﻿namespace Shared.DTOs.UserManagement;
+﻿using Shared.Enums.UserManagement;
+
+namespace Shared.DTOs.UserManagement;
 
 public class InvitedUserDto
 {
     public required string Email { get; set; }
     public required int InvitedById { get; set; }
-    public required string Status { get; set; }
+    public required InvitedUserStatus Status { get; set; }
 }
