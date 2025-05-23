46236c373328c5796d3540aa3139300add04bb80
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-23 18:40:30 -0400

    add InviteUserRequest class for user invitation management

---
+++ Shared/Requests/UserManagement/InviteUserRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Requests.UserManagement;
+
+public class InviteUserRequest
+{
+    public required string Email { get; set; }
+    public required string InvitedByEmployeeId { get; set; }
+}
