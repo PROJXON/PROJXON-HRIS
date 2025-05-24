4e02a4c99bf34d9deec0676829abff3463f283c8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 15:11:29 -0400

    add custom AuthException class for handling authentication errors

---
+++ CloudSync/Modules/UserManagement/Services/AuthException.cs
@@ -0,0 +1,6 @@
+﻿namespace CloudSync.Modules.UserManagement.Services;
+
+public class AuthException(string message, int statusCode = 400) : Exception(message)
+{
+    public int StatusCode { get; } = statusCode;
+}
