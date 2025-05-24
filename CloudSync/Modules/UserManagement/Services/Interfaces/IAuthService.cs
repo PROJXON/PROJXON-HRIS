ed233340010320a2712ceea0867e1fce80bff695
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 19:33:17 -0400

    implement authentication service with Google login and JWT generation

---
+++ CloudSync/Modules/UserManagement/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Google.Apis.Auth;
+using Shared.Requests.UserManagement;
+using Shared.Responses.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Services.Interfaces;
+
+public interface IAuthService
+{
+    Task<GoogleLoginResponse> LoginAsync(GoogleLoginRequest request);
+    Task<GoogleJsonWebSignature.Payload> ValidateIdToken(GoogleLoginRequest request);
+    string GenerateJwt(string email);
+}
