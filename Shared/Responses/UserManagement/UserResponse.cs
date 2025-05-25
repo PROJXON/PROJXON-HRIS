8b0ba1ff7d829a0f59b1b01c2e5b1279df4ea0a2
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 13:28:35 -0400

    add UserResponse class for user management responses

---
+++ Shared/Responses/UserManagement/UserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Responses.UserManagement;
+
+public class UserResponse
+{
+    public int Id { get; set; }
+    public required string Email { get; set; }
+    public DateTime CreateDateTime { get; set; }
+    public DateTime? LastLoginDateTime { get; set; }
+    public string? UserSettings { get; set; }
+}
