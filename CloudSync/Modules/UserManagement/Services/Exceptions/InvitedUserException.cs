b490cfafbf1483e50acfc1b9eebdd0ea34fe4768
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 20:57:59 -0400

    add InvitedUserException class for handling invited user errors

---
+++ CloudSync/Modules/UserManagement/Services/Exceptions/InvitedUserException.cs
@@ -0,0 +1,6 @@
+﻿namespace CloudSync.Modules.UserManagement.Services.Exceptions;
+
+public class InvitedUserException(string message, int statusCode = 400) : Exception(message)
+{
+    public int StatusCode { get; } = statusCode;
+}
