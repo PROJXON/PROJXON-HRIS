185192424c325dee2a09e26a3c89f9d884119600
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 14:57:42 -0400

    add UserRepository for user management operations

---
+++ CloudSync/Modules/UserManagement/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using CloudSync.Infrastructure;
+using CloudSync.Modules.UserManagement.Models;
+using CloudSync.Modules.UserManagement.Repositories.Interfaces;
+using CloudSync.Modules.UserManagement.Services.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Shared.DTOs.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Repositories;
+
+public class UserRepository(DatabaseContext context) : IUserRepository
+{
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        try
+        {
+            return await context.Users.ToListAsync();
+        }
+        catch (Exception e)
+        {
+            throw new UserException(e.Message, 500);
+        }
+    }
+
+    public Task<User> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateAsync(UserDto userDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateAsync(UserDto userDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAsync(string googleUserId)
+    {
+        throw new NotImplementedException();
+    }
+}
