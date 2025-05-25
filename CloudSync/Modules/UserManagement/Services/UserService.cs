81e6c384571b17ca063c6db11f5190f2d505a2fe
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 19:20:02 -0400

    add UserService for managing user data retrieval and responses

---
+++ CloudSync/Modules/UserManagement/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using CloudSync.Modules.UserManagement.Repositories.Interfaces;
+using CloudSync.Modules.UserManagement.Services.Interfaces;
+using Shared.Responses.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Services;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public async Task<IEnumerable<UserResponse>> GetAllAsync()
+    {
+        var userList = await userRepository.GetAllAsync();
+        List<UserResponse> userResponseList = [];
+            
+        userResponseList.AddRange(userList.Select(user => new UserResponse
+        {
+            Id = user.Id,
+            Email = user.Email,
+            CreateDateTime = user.CreateDateTime,
+            LastLoginDateTime = user.LastLoginDateTime,
+            UserSettings = user.UserSettings
+        }));
+
+        return userResponseList;
+    }
+
+    public async Task<UserResponse> GetByIdAsync(int id)
+    {
+        var user = await userRepository.GetByIdAsync(id);
+        
+        return new UserResponse
+        {
+            Id = user.Id,
+            Email = user.Email,
+            CreateDateTime = user.CreateDateTime,
+            LastLoginDateTime = user.LastLoginDateTime,
+            UserSettings = user.UserSettings
+        };
+    }
+
+    public async Task<UserResponse> UpdateUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteUserAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
