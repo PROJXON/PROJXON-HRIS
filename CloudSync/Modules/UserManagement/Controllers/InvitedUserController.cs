c319408dea390f734bef12a2982c3020d7f3856a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 21:15:13 -0400

    add InvitedUserController for managing user invitations

---
+++ CloudSync/Modules/UserManagement/Controllers/InvitedUserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Requests.UserManagement;
+using Shared.Responses.UserManagement;
+using CloudSync.Modules.UserManagement.Services;
+using CloudSync.Modules.UserManagement.Services.Exceptions;
+
+namespace CloudSync.Modules.UserManagement.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InvitedUserController(IInvitedUserService invitedUserService) : ControllerBase
+{
+    [HttpPost("invite-user")]
+    public async Task<ActionResult<InviteUserResponse>> InviteUser(InviteUserRequest request)
+    {
+        try
+        {
+            var response = await invitedUserService.InviteUser(request);
+            return Ok(response);
+        }
+        catch (InvitedUserException e)
+        {
+            return StatusCode(e.StatusCode, new { message = e.Message });
+        }
+    }
+}
