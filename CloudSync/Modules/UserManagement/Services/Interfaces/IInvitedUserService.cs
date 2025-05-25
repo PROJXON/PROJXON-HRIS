b29f068acf92e3c67be43162e8bbe818e73e6e63
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 21:31:55 -0400

    add IInvitedUserService interface for user invitation functionality

---
+++ CloudSync/Modules/UserManagement/Services/Interfaces/IInvitedUserService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Requests.UserManagement;
+using Shared.Responses.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Services.Interfaces;
+
+public interface IInvitedUserService
+{
+    Task<ActionResult<InviteUserResponse>> InviteUser(InviteUserRequest request);
+}
