928691e55a5e6be72c93c173bc87bbbf3ca3d92b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 15:46:46 -0400

    refactor IUserRepository to use Task for Create, Update, and Delete methods

--- CloudSync/Modules/UserManagement/Repositories/Interfaces/IUserRepository.cs
+++ CloudSync/Modules/UserManagement/Repositories/Interfaces/IUserRepository.cs
@@ -6,8 +6,8 @@ namespace CloudSync.Modules.UserManagement.Repositories.Interfaces;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllAsync();
-    Task<User> GetByIdAsync(int id);
-    void CreateAsync(UserDto userDto);
-    void UpdateAsync(UserDto userDto);
-    void DeleteAsync(string googleUserId);
+    Task<User?> GetByIdAsync(int id);
+    Task CreateAsync(UserDto userDto);
+    Task UpdateAsync(UserDto userDto);
+    Task DeleteAsync(string googleUserId);
 }
