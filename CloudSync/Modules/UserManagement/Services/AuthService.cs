4ccc1e91f9014f7a81871f8d3db2033d9ece0c05
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 09:38:49 -0400

    fix IInvitedUserRepository reference for UserManagement repository in AuthService

--- CloudSync/Modules/UserManagement/Services/AuthService.cs
+++ CloudSync/Modules/UserManagement/Services/AuthService.cs
@@ -1,7 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using CloudSync.Modules.UserManagement.Repositories;
+using CloudSync.Modules.UserManagement.Repositories.Interfaces;
 using CloudSync.Modules.UserManagement.Services.Exceptions;
 using CloudSync.Modules.UserManagement.Services.Interfaces;
 using Microsoft.IdentityModel.Tokens;
