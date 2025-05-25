c3e0ec8479953cb45df2e878c5c6305416899b9b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 11:25:37 -0400

    append Async to IInvitedUserRepository method names for consistency and clarity

--- CloudSync/Modules/UserManagement/Repositories/Interfaces/IInvitedUserRepository.cs
+++ CloudSync/Modules/UserManagement/Repositories/Interfaces/IInvitedUserRepository.cs
@@ -6,7 +6,7 @@ namespace CloudSync.Modules.UserManagement.Repositories.Interfaces;
 public interface IInvitedUserRepository
 {
     Task<IEnumerable<InvitedUser>> GetAllAsync();
-    Task<InvitedUser?> GetByEmail(string email);
-    Task<InvitedUser> Add(InvitedUserDto invitedUserDto);
-    Task DeleteInviteAsync(string id);
+    Task<InvitedUser?> GetByEmailAsync(string email);
+    Task<InvitedUser> AddAsync(InvitedUserDto invitedUserDto);
+    Task DeleteAsync(int id);
 }
