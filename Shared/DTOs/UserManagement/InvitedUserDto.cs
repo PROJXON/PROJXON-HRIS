17a94a7c3765da50a1d8954dcba8285d539a5177
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 21:44:49 -0400

    update InvitedUserDto to require Status property as a string instead of an InvitedUserStatus enum

--- Shared/DTOs/UserManagement/InvitedUserDto.cs
+++ Shared/DTOs/UserManagement/InvitedUserDto.cs
@@ -7,6 +7,6 @@ public class InvitedUserDto
     public int? Id { get; set; }
     public required string Email { get; set; }
     public required string InvitedByEmployeeId { get; set; }
-    public InvitedUserStatus Status { get; set; }
+    public required string Status { get; set; }
     public DateTime? CreateDateTime { get; set; }
 }
