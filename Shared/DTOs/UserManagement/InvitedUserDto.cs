114308e65b8275099ac0b2411e9dc6eb3209fff4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 09:52:11 -0400

    remove unnecessary properties CreateDateTime and Id from InvitedUserDto for cleaner data model

--- Shared/DTOs/UserManagement/InvitedUserDto.cs
+++ Shared/DTOs/UserManagement/InvitedUserDto.cs
@@ -4,9 +4,7 @@ namespace Shared.DTOs.UserManagement;
 
 public class InvitedUserDto
 {
-    public int? Id { get; set; }
     public required string Email { get; set; }
     public required string InvitedByEmployeeId { get; set; }
     public required string Status { get; set; }
-    public DateTime? CreateDateTime { get; set; }
 }
