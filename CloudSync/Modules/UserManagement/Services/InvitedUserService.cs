4df310d48821feba69de3601e7b5121c37fe3d94
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 10:33:15 -0400

    add InvitedUserService for managing invited users and handling invitations

---
+++ CloudSync/Modules/UserManagement/Services/InvitedUserService.cs
@@ -0,0 +1,57 @@
+﻿using CloudSync.Modules.UserManagement.Repositories.Interfaces;
+using CloudSync.Modules.UserManagement.Services.Exceptions;
+using CloudSync.Modules.UserManagement.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs.UserManagement;
+using Shared.Enums.UserManagement;
+using Shared.Requests.UserManagement;
+using Shared.Responses.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Services;
+
+public class InvitedUserService (IInvitedUserRepository invitedUserRepository) : IInvitedUserService
+{
+    public async Task<IEnumerable<InviteUserResponse>> GetAllAsync()
+    {
+        var invitedUserList = await invitedUserRepository.GetAllAsync();
+        List<InviteUserResponse> invitedUserResponseList = [];
+            
+        invitedUserResponseList.AddRange(invitedUserList.Select(invitedUser => new InviteUserResponse
+        {
+            Id = invitedUser.Id,
+            Email = invitedUser.Email,
+            InvitedByEmployeeId = invitedUser.InvitedByEmployeeId,
+            Status = Enum.Parse<InvitedUserStatus>(invitedUser.Status),
+            CreateDateTime = invitedUser.CreateDateTime,
+        }));
+
+        return invitedUserResponseList;
+    }
+    
+    public async Task<ActionResult<InviteUserResponse>> InviteUserAsync(InviteUserRequest request)
+    {
+        var existingInvite = await invitedUserRepository.GetByEmail(request.Email);
+        if (existingInvite != null)
+        {
+            throw new InvitedUserException("Email has already been invited.", 409);
+        }
+        
+        var invitedUserDto = new InvitedUserDto
+        {
+            Email = request.Email,
+            InvitedByEmployeeId = request.InvitedByEmployeeId,
+            Status = nameof(InvitedUserStatus.Pending)
+        };
+
+        var newInvitedUser = await invitedUserRepository.Add(invitedUserDto);
+        
+        return new InviteUserResponse
+        {
+            Id = newInvitedUser.Id,
+            Email = newInvitedUser.Email,
+            InvitedByEmployeeId = newInvitedUser.InvitedByEmployeeId,
+            Status = Enum.Parse<InvitedUserStatus>(newInvitedUser.Status),
+            CreateDateTime = newInvitedUser.CreateDateTime
+        };
+    }
+}
