2834d8420d731302783aeee947ec45db15ade0df
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 14:55:42 -0400

    add IUserRepository interface for user management operations

---
+++ CloudSync/Modules/UserManagement/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using CloudSync.Modules.UserManagement.Models;
+using Shared.DTOs.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<User> GetByIdAsync(int id);
+    void CreateAsync(UserDto userDto);
+    void UpdateAsync(UserDto userDto);
+    void DeleteAsync(string googleUserId);
+}
