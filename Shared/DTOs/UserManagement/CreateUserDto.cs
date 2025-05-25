d1518cde3996d8dde4db6d06160620dcad566e6f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 13:01:23 -0400

    make GoogleUserId and Email properties required in CreateUserDto, remove Id property

--- Shared/DTOs/UserManagement/CreateUserDto.cs
+++ Shared/DTOs/UserManagement/CreateUserDto.cs
@@ -2,9 +2,8 @@
 
 public class CreateUserDto
 {
-    public int Id { get; set; }
-    public string GoogleUserId { get; set; }
-    public string Email { get; set; }
+    public required string GoogleUserId { get; set; }
+    public required string Email { get; set; }
     public DateTime CreateDateTime { get; set; }
     public DateTime LastLoginDateTime { get; set; }
     public string? UserSettings { get; set; }
