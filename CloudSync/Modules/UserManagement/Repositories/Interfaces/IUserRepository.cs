074e4dd1ac16fe457008b92f84f3825e3aa7d4c0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 19:20:43 -0400

    fix: change GetByIdAsync to return a non-nullable User type

--- CloudSync/Modules/UserManagement/Repositories/Interfaces/IUserRepository.cs
+++ CloudSync/Modules/UserManagement/Repositories/Interfaces/IUserRepository.cs
@@ -6,7 +6,7 @@ namespace CloudSync.Modules.UserManagement.Repositories.Interfaces;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllAsync();
-    Task<User?> GetByIdAsync(int id);
+    Task<User> GetByIdAsync(int id);
     Task CreateAsync(UserDto userDto);
     Task UpdateAsync(UserDto userDto);
     Task DeleteAsync(string googleUserId);
