bad13ef88dd921064214c6b991891ae127dbf7e9
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-24 21:04:00 -0400

    add optional Id property to InvitedUserDto for improved user tracking

--- Shared/DTOs/UserManagement/InvitedUserDto.cs
+++ Shared/DTOs/UserManagement/InvitedUserDto.cs
@@ -4,6 +4,7 @@ namespace Shared.DTOs.UserManagement;
 
 public class InvitedUserDto
 {
+    public int? Id { get; set; }
     public required string Email { get; set; }
     public required string InvitedByEmployeeId { get; set; }
     public InvitedUserStatus Status { get; set; }
