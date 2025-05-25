dbb7972c44845bda454566d2e4cb49e15bc0c219
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 14:58:10 -0400

    add IUserService interface for user management operations

---
+++ CloudSync/Modules/UserManagement/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Responses.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<IEnumerable<UserResponse>> GetAllAsync();
+    Task<UserResponse> GetUserByIdAsync();
+    Task DeleteUserAsync();
+}
