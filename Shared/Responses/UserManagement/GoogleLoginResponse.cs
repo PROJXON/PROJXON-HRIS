c4e4e3cfc99e99a041fa3d933d58c51a1bca7df0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-25 20:02:53 -0400

    add GoogleLoginResponse class for handling Google login responses

---
+++ Shared/Responses/UserManagement/GoogleLoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Responses.UserManagement;
+
+public class GoogleLoginResponse
+{
+    public required string JsonWebToken { get; set; }
+    public UserResponse? User { get; set; }
+}
