934fc36a2ddf3ede8cc1334a0a5e24e3a53890ce
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-26 17:05:49 -0400

    update DatabaseContextModelSnapshot to change InvitedByEmployeeId type to int and set Id as the primary key for User entity

--- CloudSync/Migrations/20250526210533_SetUserPkBackToId.Designer.cs
+++ CloudSync/Migrations/20250526210533_SetUserPkBackToId.Designer.cs
@@ -5,6 +5,7 @@ using CloudSync.Infrastructure;
 using CloudSync.Modules.EmployeeManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CloudSync.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250526210533_SetUserPkBackToId")]
+    partial class SetUserPkBackToId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CloudSync/Migrations/20250526210533_SetUserPkBackToId.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetUserPkBackToId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvitedUsers_Users_InvitedByEmployeeId",
+                table: "InvitedUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InvitedByEmployeeId",
+                table: "InvitedUsers",
+                type: "integer",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvitedUsers_Users_InvitedByEmployeeId",
+                table: "InvitedUsers",
+                column: "InvitedByEmployeeId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvitedUsers_Users_InvitedByEmployeeId",
+                table: "InvitedUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvitedByEmployeeId",
+                table: "InvitedUsers",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "GoogleUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvitedUsers_Users_InvitedByEmployeeId",
+                table: "InvitedUsers",
+                column: "InvitedByEmployeeId",
+                principalTable: "Users",
+                principalColumn: "GoogleUserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
