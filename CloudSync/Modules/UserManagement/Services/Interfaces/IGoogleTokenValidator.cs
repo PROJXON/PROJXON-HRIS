bc057f11735982fd97c06138687fe98fc5f406ef
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-26 21:16:18 -0400

    add IGoogleTokenValidator interface for Google token validation

---
+++ CloudSync/Modules/UserManagement/Services/Interfaces/IGoogleTokenValidator.cs
@@ -0,0 +1,9 @@
+﻿using Google.Apis.Auth;
+using Shared.Requests.UserManagement;
+
+namespace CloudSync.Modules.UserManagement.Services.Interfaces;
+
+public interface IGoogleTokenValidator
+{
+    Task<GoogleJsonWebSignature.Payload> ValidateAsync(GoogleLoginRequest request);
+}
