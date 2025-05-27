aef4dc7399ba3700509941c886a81d2aeefd6649
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-27 00:22:18 -0400

    update UserResponse to make LastLoginDateTime a non-nullable property

--- Shared/Responses/UserManagement/UserResponse.cs
+++ Shared/Responses/UserManagement/UserResponse.cs
@@ -5,6 +5,6 @@ public class UserResponse
     public int Id { get; set; }
     public required string Email { get; set; }
     public DateTime CreateDateTime { get; set; }
-    public DateTime? LastLoginDateTime { get; set; }
+    public DateTime LastLoginDateTime { get; set; }
     public string? UserSettings { get; set; }
 }
