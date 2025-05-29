4e3d8510416df674a76df20d33216649c12f8f74
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 00:30:39 -0400

    add EmployeeDto properties

--- Shared/EmployeeManagement/Dtos/EmployeeDto.cs
+++ Shared/EmployeeManagement/Dtos/EmployeeDto.cs
@@ -1,6 +1,49 @@
-﻿namespace Shared.EmployeeManagement.Dtos;
+﻿using Shared.EmployeeManagement.Enums;
+using Shared.Enums.UserManagement;
+
+namespace Shared.EmployeeManagement.Dtos;
 
 public class EmployeeDto
 {
-    
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? ProjxonEmail { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public string? PersonalEmail { get; set; }
+    public string? EmergencyContactName { get; set; }
+    public string? EmergencyContactPhone { get; set; }
+    public string? BirthCountry { get; set; }
+    public string? BirthState { get; set; }
+    public string? BirthCity { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public string? Gender { get; set; }
+    public string? PreferredPronouns { get; set; }
+    public string? PreferredName { get; set; }
+    public string? JobTitle { get; set; }
+    public int? DepartmentId { get; set; }
+    public int? ManagerId { get; set; }
+    public int? CoachId { get; set; }
+    public HierarchyLevel? HierarchyLevel { get; set; }
+    public DateTime? OnboardingDate { get; set; }
+    public DateTime? OffboardingDate { get; set; }
+    public EmployeeStatus? Status { get; set; }
+    public string? ProfilePictureUrl { get; set; }
+    public string? ResumeUrl { get; set; }
+    public string? CoverLetterUrl { get; set; }
+    public string? LinkedInUrl { get; set; }
+    public string? GitHubUrl { get; set; }
+    public string? PersonalWebsiteUrl { get; set; }
+    public string? EducationLevel { get; set; }
+    public List<string>? UniversitiesAttended { get; set; }
+    public List<string>? DegreesEarned { get; set; }
+    public string? TimeZone { get; set; }
+    public string? Country { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? RecruitingSource { get; set; }
+    public List<string>? CanvasCoursesCompleted { get; set; }
+    public List<string>? CanvasCertificates { get; set; }
+    public string? NewCompany { get; set; }
+    public List<string>? OnboardingChecklist { get; set; }
 }
