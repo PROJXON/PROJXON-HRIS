76b6caa53efea4791e1ada1e304add32b5b10cd6
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-28 23:43:39 -0400

    refactor UserController to improve code readability and structure

--- CloudSync/Modules/UserManagement/Controllers/UserController.cs
+++ CloudSync/Modules/UserManagement/Controllers/UserController.cs
@@ -4,67 +4,60 @@ using CloudSync.Modules.UserManagement.Services.Interfaces;
 using Shared.DTOs.UserManagement;
 using Shared.Responses.UserManagement;
 
-namespace CloudSync.Modules.UserManagement.Controllers
+namespace CloudSync.Modules.UserManagement.Controllers;
+
+[Route("api/[controller]")] [ApiController] public class UserController(IUserService userService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController(IUserService userService) : ControllerBase
+    [HttpGet] public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+        try
         {
-            try
-            {
-                var response = await userService.GetAllAsync();
-                return Ok(response);
-            }
-            catch (UserException e)
-            {
-                return StatusCode(e.StatusCode, new { message = e.Message });
-            }
+            var response = await userService.GetAllAsync();
+            return Ok(response);
         }
-        
-        [HttpGet("{id}")]
-        public async Task<ActionResult<UserResponse>> GetUser(int id)
+        catch (UserException e)
         {
-            try
-            {
-                var response = await userService.GetByIdAsync(id);
-                return Ok(response);
-            }
-            catch (UserException e)
-            {
-                return StatusCode(e.StatusCode, new { message = e.Message });
-            }
-        
+            return StatusCode(e.StatusCode, new { message = e.Message });
         }
-        
-        [HttpPut("{id}")]
-        public async Task<ActionResult> PutUser(int id, [FromBody] UserDto userDto)
+    }
+    
+    [HttpGet("{id}")] public async Task<ActionResult<UserResponse>> GetUser(int id)
+    {
+        try
         {
-            try
-            {
-                await userService.UpdateAsync(id, userDto);
-                return NoContent();
-            }
-            catch (UserException e)
-            {
-                return StatusCode(e.StatusCode, new { message = e.Message });
-            }
+            var response = await userService.GetByIdAsync(id);
+            return Ok(response);
         }
-        
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteUser(int id)
+        catch (UserException e)
         {
-            try
-            {
-                await userService.DeleteAsync(id);
-                return NoContent();
-            }
-            catch (UserException e)
-            {
-                return StatusCode(e.StatusCode, new { message = e.Message });
-            }
+            return StatusCode(e.StatusCode, new { message = e.Message });
+        }
+    }
+    
+    [HttpPut("{id}")] public async Task<ActionResult> PutUser(int id, [FromBody] UserDto userDto)
+    {
+        try
+        {
+            await userService.UpdateAsync(id, userDto);
+            return NoContent();
+        }
+        catch (UserException e)
+        {
+            return StatusCode(e.StatusCode, new { message = e.Message });
+        }
+    }
+    
+    [HttpDelete("{id}")] public async Task<ActionResult> DeleteUser(int id)
+    {
+        try
+        {
+            await userService.DeleteAsync(id);
+            return NoContent();
+        }
+        catch (UserException e)
+        {
+            return StatusCode(e.StatusCode, new { message = e.Message });
         }
     }
 }
+
