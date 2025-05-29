7a84593757d62201312cf2621c286ee9cc94acd9
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 18:18:52 -0400

    implement EmployeeController with GetAllEmployees method and error handling

--- CloudSync/Modules/EmployeeManagement/Controllers/EmployeeController.cs
+++ CloudSync/Modules/EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CloudSync.Modules.EmployeeManagement.Services.Exceptions;
+using CloudSync.Modules.EmployeeManagement.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.EmployeeManagement.Responses;
 
 namespace CloudSync.Modules.EmployeeManagement.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UserController() : ControllerBase
+public class EmployeeController(IEmployeeService employeeService) : ControllerBase
 {
-        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeResponse>>> GetAllEmployees()
+        {
+                try
+                {
+                        var response = await employeeService.GetAllAsync();
+                        return Ok(response);
+                }
+                catch (EmployeeException e)
+                {
+                        return StatusCode(e.StatusCode, e.Message);
+                }
+        }
 }
 
