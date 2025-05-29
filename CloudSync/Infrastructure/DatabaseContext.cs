1b5606c8005da1e78b30e69acad89a27806f9aa4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 14:35:17 -0400

    update UserRole and Department IDs which were set to be 0-indexed in DatabaseContext

--- CloudSync/Infrastructure/DatabaseContext.cs
+++ CloudSync/Infrastructure/DatabaseContext.cs
@@ -39,9 +39,9 @@ public class DatabaseContext : DbContext
         modelBuilder.Entity<UserRole>()
             .HasKey(u => u.Id);
         modelBuilder.Entity<UserRole>().HasData(
-            new UserRole { Id = 0, Name = "Administrator" },
-            new UserRole { Id = 1, Name = "Human Resources" },
-            new UserRole { Id = 2, Name = "Employee" },
+            new UserRole { Id = 1, Name = "Administrator" },
+            new UserRole { Id = 2, Name = "Human Resources" },
+            new UserRole { Id = 3, Name = "Employee" },
             new UserRole { Id = 4, Name = "Guest" }
         );
         
@@ -54,31 +54,31 @@ public class DatabaseContext : DbContext
         modelBuilder.Entity<Department>()
             .HasKey(u => u.Id);
         modelBuilder.Entity<Department>().HasData(
-            new Department { Id = 0, Name = "Business" },
-            new Department { Id = 6, Name = "Executive", ParentDepartmentId = 0},
-            new Department { Id = 7, Name = "Consulting", ParentDepartmentId = 0},
+            new Department { Id = 1, Name = "Business" },
+            new Department { Id = 6, Name = "Executive", ParentDepartmentId = 1},
+            new Department { Id = 7, Name = "Consulting", ParentDepartmentId = 1 },
             
-            new Department { Id = 1, Name = "Human Resources" },
-            new Department { Id = 8, Name = "Onboarding", ParentDepartmentId = 1},
-            new Department { Id = 9, Name = "Offboarding", ParentDepartmentId = 1 },
-            new Department { Id = 10, Name = "Recruiting", ParentDepartmentId = 1 },
+            new Department { Id = 2, Name = "Human Resources" },
+            new Department { Id = 8, Name = "Onboarding", ParentDepartmentId = 2 },
+            new Department { Id = 9, Name = "Offboarding", ParentDepartmentId = 2 },
+            new Department { Id = 10, Name = "Recruiting", ParentDepartmentId = 2 },
             
-            new Department { Id = 2, Name = "Information Technology" },
-            new Department { Id = 11, Name = "AI", ParentDepartmentId = 2 },
-            new Department { Id = 12, Name = "Applications Development", ParentDepartmentId = 2 },
-            new Department { Id = 13, Name = "Cybersecurity", ParentDepartmentId = 2 },
-            new Department { Id = 14, Name = "SEO", ParentDepartmentId = 2 },
-            new Department { Id = 15, Name = "Web Development", ParentDepartmentId = 2 },
+            new Department { Id = 3, Name = "Information Technology" },
+            new Department { Id = 11, Name = "AI", ParentDepartmentId = 3 },
+            new Department { Id = 12, Name = "Applications Development", ParentDepartmentId = 3 },
+            new Department { Id = 13, Name = "Cybersecurity", ParentDepartmentId = 3 },
+            new Department { Id = 14, Name = "SEO", ParentDepartmentId = 3 },
+            new Department { Id = 15, Name = "Web Development", ParentDepartmentId = 3 },
             
-            new Department { Id = 3, Name = "Marketing" },
-            new Department { Id = 16, Name = "Market Research", ParentDepartmentId = 3 },
-            new Department { Id = 17, Name = "Social Media", ParentDepartmentId = 3 },
-            new Department { Id = 18, Name = "Copywriting", ParentDepartmentId = 3 },
-            new Department { Id = 19, Name = "Graphic Design", ParentDepartmentId = 3 },
+            new Department { Id = 4, Name = "Marketing" },
+            new Department { Id = 16, Name = "Market Research", ParentDepartmentId = 4 },
+            new Department { Id = 17, Name = "Social Media", ParentDepartmentId = 4 },
+            new Department { Id = 18, Name = "Copywriting", ParentDepartmentId = 4 },
+            new Department { Id = 19, Name = "Graphic Design", ParentDepartmentId = 4 },
             
-            new Department { Id = 4, Name = "Operations" },
-            new Department { Id = 20, Name = "Finance", ParentDepartmentId = 4 },
-            new Department { Id = 21, Name = "Legal", ParentDepartmentId = 4 }
+            new Department { Id = 5, Name = "Operations" },
+            new Department { Id = 20, Name = "Finance", ParentDepartmentId = 5 },
+            new Department { Id = 21, Name = "Legal", ParentDepartmentId = 5 }
         );
         
         modelBuilder.Entity<ProjectTeam>()
