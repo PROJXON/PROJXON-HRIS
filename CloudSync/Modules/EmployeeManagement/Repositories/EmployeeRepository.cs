40d6765dd534e1d3ab980c43eca4477184b01d11
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 15:25:48 -0400

    implement IEmployeeRepository and IEmployeeService with CRUD method stubs

--- CloudSync/Modules/EmployeeManagement/Repositories/EmployeeRepository.cs
+++ CloudSync/Modules/EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace CloudSync.Modules.EmployeeManagement.Repositories;
+﻿using CloudSync.Infrastructure;
+using CloudSync.Modules.EmployeeManagement.Models;
+using CloudSync.Modules.EmployeeManagement.Repositories.Interfaces;
+using Shared.EmployeeManagement.Dtos;
 
-public class EmployeeRepository
+namespace CloudSync.Modules.EmployeeManagement.Repositories;
+
+public class EmployeeRepository(DatabaseContext context) : IEmployeeRepository
 {
-    
+    public async Task<IEnumerable<Employee>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Employee>> GetByDepartmentAsync(string department)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Employee>> GetByRoleAsync(string role)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Employee> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Employee> CreateAsync(EmployeeDto employeeDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(int id, EmployeeDto employeeDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- CloudSync/Modules/EmployeeManagement/Services/EmployeeService.cs
+++ CloudSync/Modules/EmployeeManagement/Services/EmployeeService.cs
@@ -1,6 +1,45 @@
-﻿namespace CloudSync.Modules.EmployeeManagement.Services;
+﻿using AutoMapper;
+using CloudSync.Modules.EmployeeManagement.Repositories.Interfaces;
+using CloudSync.Modules.EmployeeManagement.Services.Interfaces;
+using Shared.EmployeeManagement.Requests;
+using Shared.EmployeeManagement.Responses;
 
-public class EmployeeService
+namespace CloudSync.Modules.EmployeeManagement.Services;
+
+public class EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper) : IEmployeeService
 {
-    
+    public async Task<IEnumerable<EmployeeResponse>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<EmployeeResponse>> GetByDepartmentAsync(string department)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<EmployeeResponse>> GetByRoleAsync(string role)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<EmployeeResponse> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<EmployeeResponse> CreateAsync(CreateEmployeeRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(int id, UpdateEmployeeRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
