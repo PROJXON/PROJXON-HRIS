7e4b4ef8cd16db8baaa5bc04f15237c9b8341b1d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 17:45:37 -0400

    add ManagerOrCoachSummary model with Id, FirstName, and LastName properties

---
+++ Shared/EmployeeManagement/Responses/ManagerOrCoachSummary.cs
@@ -0,0 +1,8 @@
+namespace Shared.EmployeeManagement.Responses;
+
+public class ManagerOrCoachSummary
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
