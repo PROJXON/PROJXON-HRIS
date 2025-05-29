44e8552fa00c4867a1c737cd061b4502bbb1b847
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 00:06:29 -0400

    refactor EmployeeException to include status code in constructor

--- CloudSync/Modules/EmployeeManagement/Services/Exceptions/EmployeeException.cs
+++ CloudSync/Modules/EmployeeManagement/Services/Exceptions/EmployeeException.cs
@@ -1,6 +1,6 @@
 ﻿namespace CloudSync.Modules.EmployeeManagement.Services.Exceptions;
 
-public class EmployeeException
+public class EmployeeException(string message, int statusCode = 400) : Exception(message)
 {
-    
+    public int StatusCode { get; } = statusCode;
 }
