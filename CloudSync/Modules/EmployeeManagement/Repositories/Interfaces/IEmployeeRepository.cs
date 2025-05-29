b224c957e3265a1560bad2c4372230f35673d32d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-29 15:21:29 -0400

    add methods to IEmployeeRepository for CRUD operations and querying by department and role

--- CloudSync/Modules/EmployeeManagement/Repositories/Interfaces/IEmployeeRepository.cs
+++ CloudSync/Modules/EmployeeManagement/Repositories/Interfaces/IEmployeeRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace CloudSync.Modules.EmployeeManagement.Repositories.Interfaces;
+﻿using CloudSync.Modules.EmployeeManagement.Models;
+using Shared.EmployeeManagement.Dtos;
+
+namespace CloudSync.Modules.EmployeeManagement.Repositories.Interfaces;
 
 public interface IEmployeeRepository
 {
-    
+    Task<IEnumerable<Employee>> GetAllAsync();
+    Task<IEnumerable<Employee>> GetByDepartmentAsync(string department);
+    Task<IEnumerable<Employee>> GetByRoleAsync(string role);
+    Task<Employee> GetByIdAsync(int id);
+    Task<Employee> CreateAsync(EmployeeDto employeeDto);
+    Task UpdateAsync(int id, EmployeeDto employeeDto);
+    Task DeleteAsync(int id);
 }
