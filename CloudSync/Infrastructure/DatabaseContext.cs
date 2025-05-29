aacbaa1535b5a0d997c4336a41d12545348df2c8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-05-28 23:32:28 -0400

    add initial department data to DatabaseContext for employee management

--- CloudSync/Infrastructure/DatabaseContext.cs
+++ CloudSync/Infrastructure/DatabaseContext.cs
@@ -48,6 +48,33 @@ public class DatabaseContext : DbContext
         
         modelBuilder.Entity<Department>()
             .HasKey(u => u.Id);
+        modelBuilder.Entity<Department>().HasData(
+            new Department { Id = 0, Name = "Business" },
+            new Department { Id = 6, Name = "Executive", ParentDepartmentId = 0},
+            new Department { Id = 7, Name = "Consulting", ParentDepartmentId = 0},
+            
+            new Department { Id = 1, Name = "Human Resources" },
+            new Department { Id = 8, Name = "Onboarding", ParentDepartmentId = 1},
+            new Department { Id = 9, Name = "Offboarding", ParentDepartmentId = 1 },
+            new Department { Id = 10, Name = "Recruiting", ParentDepartmentId = 1 },
+            
+            new Department { Id = 2, Name = "Information Technology" },
+            new Department { Id = 11, Name = "AI", ParentDepartmentId = 2 },
+            new Department { Id = 12, Name = "Applications Development", ParentDepartmentId = 2 },
+            new Department { Id = 13, Name = "Cybersecurity", ParentDepartmentId = 2 },
+            new Department { Id = 14, Name = "SEO", ParentDepartmentId = 2 },
+            new Department { Id = 15, Name = "Web Development", ParentDepartmentId = 2 },
+            
+            new Department { Id = 3, Name = "Marketing" },
+            new Department { Id = 16, Name = "Market Research", ParentDepartmentId = 3 },
+            new Department { Id = 17, Name = "Social Media", ParentDepartmentId = 3 },
+            new Department { Id = 18, Name = "Copywriting", ParentDepartmentId = 3 },
+            new Department { Id = 19, Name = "Graphic Design", ParentDepartmentId = 3 },
+            
+            new Department { Id = 4, Name = "Operations" },
+            new Department { Id = 20, Name = "Finance", ParentDepartmentId = 4 },
+            new Department { Id = 21, Name = "Legal", ParentDepartmentId = 4 }
+        );
         
         modelBuilder.Entity<ProjectTeam>()
             .HasKey(u => u.Id);
