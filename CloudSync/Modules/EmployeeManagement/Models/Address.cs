bbd6901ea2dd0606694f724fe41ed3c786337412
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-03 12:07:26 -0400

    add Address model for employee management with validation attributes

---
+++ CloudSync/Modules/EmployeeManagement/Models/Address.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudSync.Modules.EmployeeManagement.Models;
+
+public class Address
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(80)]
+    public required string AddressLine1 { get; set; }
+    
+    [StringLength(80)]
+    public string? AddressLine2 { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public required string Country { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public required string StateOrProvince { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public required string City { get; set; }
+    
+    [Required]
+    [StringLength(15)]
+    public required string PostalCode { get; set; }
+}
