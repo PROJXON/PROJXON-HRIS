0e14687d1cf1d41a64f97e06ad03bbbe7b82a4fd
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-09 12:53:40 -0400

    add UpdateUserRequest model for user management

---
+++ Shared/UserManagement/Requests/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.UserManagement.Requests;
+
+public class UpdateUserRequest
+{
+    public int Id { get; set; }
+    public required string GoogleUserId { get; set; }
+    public required string Email { get; set; }
+    public int RoleId { get; set; } 
+    public int EmployeeId { get; set; }
+    public string? UserSettings { get; set; }
+}
