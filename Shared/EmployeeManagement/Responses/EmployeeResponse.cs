b4801212ecfd30fa6bb1d3cf26ce4446cef65ad0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-09 11:49:43 -0400

    refactor EmployeeResponse to use response types for nested entities

--- Shared/EmployeeManagement/Responses/EmployeeResponse.cs
+++ Shared/EmployeeManagement/Responses/EmployeeResponse.cs
@@ -5,14 +5,14 @@ namespace Shared.EmployeeManagement.Responses;
 public class EmployeeResponse
 {
     public int? Id { get; set; }
-    public EmployeeBasic BasicInfo { get; set; } = new();
-    public EmployeeContactInfo ContactInfo { get; set; } = new();
+    public EmployeeBasic BasicInfo { get; set; }
+    public EmployeeContactInfo ContactInfo { get; set; }
 
-    public EmployeePosition? PositionDetails { get; set; } = new();
-    public EmployeeDocuments? Documents { get; set; } = new();
-    public EmployeeLegal? Legal { get; set; } = new();
-    public EmployeeEducation? Education { get; set; } = new();
-    public EmployeeTraining? Training{ get; set; } = new();
+    public EmployeePositionResponse? PositionDetails { get; set; }
+    public EmployeeDocumentsResponse? Documents { get; set; }
+    public EmployeeLegalResponse? Legal { get; set; }
+    public EmployeeEducationResponse? Education { get; set; }
+    public EmployeeTrainingResponse? Training{ get; set; }
     public DateTime? CreateDateTime { get; set; }
     public DateTime? UpdateDateTime { get; set; }
 }
