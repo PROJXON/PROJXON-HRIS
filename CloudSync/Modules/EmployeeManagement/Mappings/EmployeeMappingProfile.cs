2a71f096193786d8517a214ac331270bc13d2635
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-09 11:42:59 -0400

    refactor Employee mapping profile to remove EmployeeDto and PositionSummary mappings, add mappings for Employee sub-models to their respective responses

--- CloudSync/Modules/EmployeeManagement/Mappings/EmployeeMappingProfile.cs
+++ CloudSync/Modules/EmployeeManagement/Mappings/EmployeeMappingProfile.cs
@@ -11,25 +11,22 @@ public class EmployeeMappingProfile : Profile
 {
     public EmployeeMappingProfile()
     {
-        CreateMap<UpdateEmployeeRequest, EmployeeDto>()
+        CreateMap<UpdateEmployeeRequest, Employee>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.CreateDateTime, opt => opt.Ignore())
             .ForMember(dest => dest.UpdateDateTime, opt => opt.Ignore());
 
-        CreateMap<EmployeeDto, Employee>();
-
-        CreateMap<Employee, EmployeeDto>();
-
-        CreateMap<EmployeeDto, EmployeeResponse>();
-
+        CreateMap<Employee, EmployeeResponse>();
+        
+        CreateMap<EmployeeDocuments, EmployeeDocumentsResponse>();
+        CreateMap<EmployeeEducation, EmployeeEducationResponse>();
+        CreateMap<EmployeeLegal, EmployeeLegalResponse>();
+        CreateMap<EmployeePosition, EmployeePositionResponse>();
+        CreateMap<EmployeeTraining, EmployeeTrainingResponse>();
+        
         CreateMap<Employee, ManagerOrCoachSummary>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(u => u.Id))
             .ForMember(dest => dest.FirstName, opt => opt.MapFrom(u => u.BasicInfo.FirstName))
             .ForMember(dest => dest.LastName, opt => opt.MapFrom(u => u.BasicInfo.LastName));
-        
-        CreateMap<EmployeePosition, PositionSummary>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(u => u.Id))
-            .ForMember(dest => dest.PositionName, opt => opt.MapFrom(u => u.PositionName))
-            .ForMember(dest => dest.HierarchyLevel, opt => opt.MapFrom(u => Enum.Parse<HierarchyLevel>(u.HierarchyLevel)));
     }
 }
