044a20f23d1cef57841b4da4ac08ffff7bb66586
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-10 10:23:15 -0400

    add EmployeeDocumentsRequest class

---
+++ Shared/EmployeeManagement/Requests/EmployeeDocumentsRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.EmployeeManagement.Requests;
+
+public class EmployeeDocumentsRequest
+{
+    public string? ProfilePictureUrl { get; set; }
+    public string? ResumeUrl { get; set; }
+    public string? CoverLetterUrl { get; set; }
+    public string? LinkedInUrl { get; set; }
+    public string? GitHubUrl { get; set; }
+    public string? PersonalWebsiteUrl { get; set; }
+}
