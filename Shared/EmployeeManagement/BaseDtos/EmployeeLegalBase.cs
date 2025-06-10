a2f9786a735ac0764394ff3d3cda442882df5947
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-10 10:39:49 -0400

    add EmployeeLegalBase class and refactor request/response classes to inherit from it

---
+++ Shared/EmployeeManagement/BaseDtos/EmployeeLegalBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.EmployeeManagement.BaseDtos;
+
+public class EmployeeLegalBase
+{
+    public string? IdNumber { get; set; }
+    public string? IdType { get; set; }
+    public string? IdCountry { get; set; }
+    public string? IdState { get; set; }
+    public string? WorkAuthorizationType { get; set; }
+    public string? WorkAuthorizationDocumentUrl { get; set; }
+    public string? VisaNumber { get; set; }
+    public DateTime? VisaExpirationDate { get; set; }
+    public string? WorkPermitNumber { get; set; }
+    public DateTime? WorkExpirationDate { get; set; }
+}
--- Shared/EmployeeManagement/Responses/EmployeeLegalResponse.cs
+++ Shared/EmployeeManagement/Responses/EmployeeLegalResponse.cs
@@ -1,15 +1,8 @@
-﻿namespace Shared.EmployeeManagement.Responses;
+﻿using Shared.EmployeeManagement.BaseDtos;
 
-public class EmployeeLegalResponse
+namespace Shared.EmployeeManagement.Responses;
+
+public class EmployeeLegalResponse : EmployeeLegalBase
 {
-    public string? IdNumber { get; set; }
-    public string? IdType { get; set; }
-    public string? IdCountry { get; set; }
-    public string? IdState { get; set; }
-    public string? WorkAuthorizationType { get; set; }
-    public string? WorkAuthorizationDocumentUrl { get; set; }
-    public string? VisaNumber { get; set; }
-    public DateTime? VisaExpirationDate { get; set; }
-    public string? WorkPermitNumber { get; set; }
-    public DateTime? WorkExpirationDate { get; set; }
+    
 }
