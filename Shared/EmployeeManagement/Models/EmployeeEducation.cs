65dc595ec3dd7f54b5303f5018ff753328e15e86
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-12 17:01:27 -0400

    Updates EmployeeEducation model
    Updates  formatting in the EmployeeEducation model.
    
    Removes unnecessary whitespace.

--- Shared/EmployeeManagement/Models/EmployeeEducation.cs
+++ Shared/EmployeeManagement/Models/EmployeeEducation.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shared.EmployeeManagement.Models;
@@ -7,15 +7,15 @@ public class EmployeeEducation
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    
+
     public Employee? Employee { get; set; }
-    
+
     [StringLength(20)]
     public string? EducationLevel { get; set; }
-    
+
     [Column(TypeName = "jsonb")]
     public List<string>? UniversitiesAttended { get; set; }
-    
+
     [Column(TypeName = "jsonb")]
     public List<string>? DegreesEarned { get; set; }
 }
