f8c84b3cae80b68a625789512f7a8f6ad22d1e12
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-12 16:48:34 -0400

    Updates employee mapping configurations
    Configures mapping profiles to ignore the Employee
    navigation property during request mapping.

--- CloudSync/Modules/EmployeeManagement/Mappings/EmployeeMappingProfile.cs
+++ CloudSync/Modules/EmployeeManagement/Mappings/EmployeeMappingProfile.cs
@@ -10,24 +10,28 @@ public class EmployeeMappingProfile : Profile
     public EmployeeMappingProfile()
     {
         CreateMap<UpdateEmployeeRequest, Employee>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.CreateDateTime, opt => opt.Ignore())
             .ForMember(dest => dest.UpdateDateTime, opt => opt.Ignore());
-        
-        CreateMap<EmployeeDocumentsRequest, EmployeeDocuments>();
-        CreateMap<EmployeeEducationRequest, EmployeeEducation>();
-        CreateMap<EmployeeLegalRequest, EmployeeLegal>();
-        CreateMap<EmployeePositionRequest, EmployeePosition>();
-        CreateMap<EmployeeTrainingRequest, EmployeeTraining>();
+
+        CreateMap<EmployeeDocumentsRequest, EmployeeDocuments>()
+            .ForMember(dest => dest.Employee, opt => opt.Ignore());
+        CreateMap<EmployeeEducationRequest, EmployeeEducation>()
+            .ForMember(dest => dest.Employee, opt => opt.Ignore());
+        CreateMap<EmployeeLegalRequest, EmployeeLegal>()
+            .ForMember(dest => dest.Employee, opt => opt.Ignore());
+        CreateMap<EmployeePositionRequest, EmployeePosition>()
+            .ForMember(dest => dest.Employee, opt => opt.Ignore());
+        CreateMap<EmployeeTrainingRequest, EmployeeTraining>()
+            .ForMember(dest => dest.Employee, opt => opt.Ignore());
 
         CreateMap<Employee, EmployeeResponse>();
-        
+
         CreateMap<EmployeeDocuments, EmployeeDocumentsResponse>();
         CreateMap<EmployeeEducation, EmployeeEducationResponse>();
         CreateMap<EmployeeLegal, EmployeeLegalResponse>();
         CreateMap<EmployeePosition, EmployeePositionResponse>();
         CreateMap<EmployeeTraining, EmployeeTrainingResponse>();
-        
+
         CreateMap<Employee, ManagerOrCoachSummary>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(u => u.Id))
             .ForMember(dest => dest.FirstName, opt => opt.MapFrom(u => u.BasicInfo.FirstName))
