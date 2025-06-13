c5710bf8ce5aaec41a84a1b7a7abf1a23b67a673
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-13 18:57:38 -0400

    rename Department to ParentDepartment

--- Shared/EmployeeManagement/Responses/DepartmentResponse.cs
+++ Shared/EmployeeManagement/Responses/DepartmentResponse.cs
@@ -5,5 +5,5 @@ namespace Shared.EmployeeManagement.Responses;
 
 public class DepartmentResponse : DepartmentBase
 {
-    public Department? Department { get; set; }
+    public Department? ParentDepartment { get; set; }
 }
