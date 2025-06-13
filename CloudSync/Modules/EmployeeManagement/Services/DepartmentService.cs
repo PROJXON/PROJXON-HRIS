39c5df85a40e96cd336b24d6ac2278014fa95250
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-13 19:18:13 -0400

    implement GetAllAsync method in DepartmentService to retrieve and map departments

--- CloudSync/Modules/EmployeeManagement/Services/DepartmentService.cs
+++ CloudSync/Modules/EmployeeManagement/Services/DepartmentService.cs
@@ -1,12 +1,19 @@
+using AutoMapper;
+using CloudSync.Modules.EmployeeManagement.Repositories.Interfaces;
 using CloudSync.Modules.EmployeeManagement.Services.Interfaces;
 using Shared.EmployeeManagement.Responses;
 
 namespace CloudSync.Modules.EmployeeManagement.Services;
 
-public class DepartmentService : IDepartmentService
+public class DepartmentService(IDepartmentRepository departmentRepository, IMapper mapper) : IDepartmentService
 {
     public async Task<IEnumerable<DepartmentResponse>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var departments = await departmentRepository.GetAllAsync();
+        List<DepartmentResponse> departmentResponseList = [];
+        
+        departmentResponseList.AddRange(departments.Select(mapper.Map<DepartmentResponse>));
+
+        return departmentResponseList;
     }
 }
