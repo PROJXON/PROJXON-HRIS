b08c7ee1d3df9756e3ed536c44e8613c58a59fa5
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-13 19:20:37 -0400

    add custom DepartmentException class for handling department-related errors

---
+++ CloudSync/Modules/EmployeeManagement/Services/Exceptions/DepartmentException.cs
@@ -0,0 +1,6 @@
+﻿namespace CloudSync.Modules.EmployeeManagement.Services.Exceptions;
+
+public class DepartmentException(string message, int statusCode = 400) : Exception(message)
+{
+    public int StatusCode { get; } = statusCode;
+}
