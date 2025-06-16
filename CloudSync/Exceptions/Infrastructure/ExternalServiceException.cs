fb05dfd620fdd85d7e216a302f676a044019c1c9
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 19:14:19 -0400

    add ExternalServiceException class with 502 status code

---
+++ CloudSync/Exceptions/Infrastructure/ExternalServiceException.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudSync.Exceptions.Infrastructure;
+
+public class ExternalServiceException : CloudSyncException
+{
+    public ExternalServiceException(string message) : base(message, 502)
+    {
+    }
+
+    public ExternalServiceException(string message, Exception innerException) : base(message, innerException, 502)
+    {
+    }
+}
