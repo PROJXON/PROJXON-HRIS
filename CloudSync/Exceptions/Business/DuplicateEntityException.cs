be630df1f423bca6c527f71e8c785a35982aa02b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 19:20:27 -0400

    add DuplicateEntityException for handling duplicate entity errors

---
+++ CloudSync/Exceptions/Business/DuplicateEntityException.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudSync.Exceptions.Business;
+
+public class DuplicateEntityException : CloudSyncException
+{
+    public DuplicateEntityException(string message) : base(message, 409)
+    {
+    }
+
+    public DuplicateEntityException(string message, Exception innerException) : base(message, innerException, 409)
+    {
+    }
+}
