2cfa1ed694ab9fa43faf223c462530cee9c5c71e
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 19:54:17 -0400

    remove Employee module specific exceptions in favor of project level exceptions

--- CloudSync/Modules/UserManagement/Services/Exceptions/InvitedUserException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CloudSync.Modules.UserManagement.Services.Exceptions;
-
-public class InvitedUserException(string message, int statusCode = 400) : Exception(message)
-{
-    public int StatusCode { get; } = statusCode;
-}
