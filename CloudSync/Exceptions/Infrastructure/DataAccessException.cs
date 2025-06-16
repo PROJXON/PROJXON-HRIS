c436c69757a30a13e648c8577ab12a7e804ac441
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 17:47:38 -0400

    add DataAccessException class for handling data access errors

---
+++ CloudSync/Exceptions/Infrastructure/DataAccessException.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudSync.Exceptions.Infrastructure;
+
+public class DataAccessException : CloudSyncException
+{
+    public DataAccessException(string message, int statusCode = 400) : base(message, statusCode)
+    {
+    }
+
+    public DataAccessException(string message, Exception innerException, int statusCode = 400) : base(message, innerException, statusCode)
+    {
+    }
+}
