b415e4c35e33cf028be6a1bd1099c18e98ce3a9a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 19:13:00 -0400

    add EntityNotFoundException for handling not found errors

---
+++ CloudSync/Exceptions/Business/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudSync.Exceptions.Business;
+
+public class EntityNotFoundException : CloudSyncException
+{
+    public EntityNotFoundException(string message) : base(message, 404)
+    {
+    }
+
+    public EntityNotFoundException(string message, Exception innerException) : base(message, innerException, 404)
+    {
+    }
+}
