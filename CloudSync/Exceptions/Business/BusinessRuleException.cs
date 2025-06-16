7e1a27d6e24db42115cc0d298356f90ce55f51a3
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 19:21:10 -0400

    add BusinessRuleException for handling policy violations

---
+++ CloudSync/Exceptions/Business/BusinessRuleException.cs
@@ -0,0 +1,13 @@
+﻿namespace CloudSync.Exceptions.Business;
+
+// Used for catching scenarios that go against company policy
+public class BusinessRuleException : CloudSyncException
+{
+    public BusinessRuleException(string message) : base(message, 400)
+    {
+    }
+
+    public BusinessRuleException(string message, Exception innerException) : base(message, innerException, 400)
+    {
+    }
+}
