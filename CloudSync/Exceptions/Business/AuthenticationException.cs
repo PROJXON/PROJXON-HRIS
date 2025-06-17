0d62a37be0a04ec9601ce8f57477aa7ad1bf3150
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 20:05:26 -0400

    add AuthenticationException class for handling authentication errors

---
+++ CloudSync/Exceptions/Business/AuthenticationException.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudSync.Exceptions.Business;
+
+public class AuthenticationException : CloudSyncException
+{
+    public AuthenticationException(string message) : base(message, 401)
+    {
+    }
+
+    public AuthenticationException(string message, Exception innerException) : base(message, innerException, 401)
+    {
+}
