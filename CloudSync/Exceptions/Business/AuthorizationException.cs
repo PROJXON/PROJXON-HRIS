f8cbec3c546a4f4351d47d0a5e0c0bc462b525e7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-16 20:01:41 -0400

    add AuthorizationException class for handling authorization errors

---
+++ CloudSync/Exceptions/Business/AuthorizationException.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudSync.Exceptions.Business;
+
+public class AuthorizationException : CloudSyncException
+{
+    public AuthorizationException(string message) : base(message, 403)
+    {
+    }
+
+    public AuthorizationException(string message, Exception innerException) : base(message, innerException, 403)
+    {
+    }
+}
