74ed1a96a19181f708c91976e73626dff6eede88
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-19 15:58:25 -0400

    refactor EmployeeContactInfo to simplify Address handling and remove AddressId property

--- CloudSync/Modules/EmployeeManagement/Models/EmployeeContactInfo.cs
+++ CloudSync/Modules/EmployeeManagement/Models/EmployeeContactInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace CloudSync.Modules.EmployeeManagement.Models;
@@ -11,16 +10,13 @@ public class EmployeeContactInfo
     [StringLength(20)]
     public string? Phone { get; set; }
     
+    public Address Address { get; set; } = new();
+    
     [StringLength(30)]
     public string? InternationalPhone { get; set; }
     
     [StringLength(30)]
     public string? InternationalPhoneType { get; set; }
-    
-    public int? AddressId { get; set; }
-    
-    [ForeignKey("AddressId")]
-    public Address? Address { get; set; }
     
     [EmailAddress]
     [StringLength(60)]
--- Shared/EmployeeManagement/BaseDtos/EmployeeContactInfoBase.cs
+++ Shared/EmployeeManagement/BaseDtos/EmployeeContactInfoBase.cs
@@ -5,7 +5,6 @@ public class EmployeeContactInfoBase
     public string? Phone { get; set; }
     public string? InternationalPhone { get; set; }
     public string? InternationalPhoneType { get; set; }
-    public int? AddressId { get; set; }
     public string? ProjxonEmail { get; set; }
     public string? PersonalEmail { get; set; }
     public string? EmergencyContactName { get; set; }
