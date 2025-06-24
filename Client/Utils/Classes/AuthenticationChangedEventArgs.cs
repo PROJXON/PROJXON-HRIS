69bedc4722c855b4aa59137361707f15f6b17d0a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-23 20:11:22 -0400

    add AuthenticationChangedEventArgs class to handle authentication state changes

---
+++ Client/Utils/Classes/AuthenticationChangedEventArgs.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Client.Utils.Classes;
+
+public class AuthenticationChangedEventArgs(bool isAuthenticated) : EventArgs
+{
+    public bool IsAuthenticated { get; } = isAuthenticated;
+}
