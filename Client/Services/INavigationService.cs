a2e10d8e56a6924cdac0c17f1ddc2481a13e0976
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-23 20:07:47 -0400

    add INavigationService and NavigationService for view model navigation

---
+++ Client/Services/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿using Client.Utils.Enums;
+
+using System;
+using Client.Utils.Classes;
+
+namespace Client.Services;
+
+public interface INavigationService
+{
+    event EventHandler<NavigationEventArgs>? NavigationRequested;
+    void NavigateTo(ViewModelType viewModelType);
+}
---
+++ Client/Services/NavigationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Client.Utils.Classes;
+using Client.Utils.Enums;
+
+namespace Client.Services;
+
+public class NavigationService : INavigationService
+{
+    public event EventHandler<NavigationEventArgs>? NavigationRequested;
+
+    public void NavigateTo(ViewModelType viewModelType)
+    {
+        NavigationRequested?.Invoke(this, new NavigationEventArgs(viewModelType));
+    }
+}
