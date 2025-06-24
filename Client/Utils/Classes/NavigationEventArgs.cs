0eae7951cbe27075f86ff7f22f4668f057d3d4c4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-23 20:07:07 -0400

    add NavigationEventArgs class for view model navigation events

---
+++ Client/Utils/Classes/NavigationEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Client.Utils.Enums;
+
+namespace Client.Utils.Classes;
+
+public class NavigationEventArgs(ViewModelType viewModelType) : EventArgs
+{
+    public ViewModelType ViewModelType { get; } = viewModelType;
+}
