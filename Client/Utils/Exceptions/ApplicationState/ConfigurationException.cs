474c3d6821dc51f079dc68b422ebcefd271ada67
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 17:27:44 -0400

    add ConfigurationException class for handling configuration errors

---
+++ Client/Utils/Exceptions/ApplicationState/ConfigurationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Client.Utils.Enums;
+
+namespace Client.Utils.Exceptions.ApplicationState;
+
+public class ConfigurationException(
+    string message,
+    string userMessage,
+    string configurationMissing,
+    Exception? innerException = null) : ClientExceptionBase(message, userMessage, ErrorSeverity.Critical,
+    ErrorCategory.Configuration, shouldReport: true,
+    isRetryable: false, suggestedActions: ["Please contact support."], innerException)
+{
+    public string ConfigurationMissing = configurationMissing;
+}
