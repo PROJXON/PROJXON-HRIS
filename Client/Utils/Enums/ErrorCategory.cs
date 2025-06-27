4c0da913dfc83fe2ac5ac7c4ca5eeae578ba1ccb
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 15:46:53 -0400

    add ErrorCategory enum to categorize application errors

---
+++ Client/Utils/Enums/ErrorCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.Utils.Enums;
+
+public enum ErrorCategory
+{
+    General,
+    Authentication,
+    Authorization,
+    Validation,
+    Network,
+    Service,
+    UserInput,
+    Configuration,
+    Data,
+    Business
+}
