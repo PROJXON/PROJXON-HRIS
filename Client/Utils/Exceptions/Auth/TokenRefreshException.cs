a572adc5238122d453c15dcaefb695c1c5fd5cbe
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 17:39:23 -0400

    add TokenRefreshException for handling session refresh errors

---
+++ Client/Utils/Exceptions/Auth/TokenRefreshException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Client.Utils.Enums;
+
+namespace Client.Utils.Exceptions.Auth;
+
+public class TokenRefreshException(
+    string message,
+    string userMessage = "Unable to refresh your session. Please sign in again.",
+    int? statusCode = null,
+    string? errorDescription = null,
+    Exception? innerException = null)
+    : AuthenticationException(message, userMessage, AuthenticationTokenType.RefreshToken, requiresRelogin: true, innerException)
+{
+    public int? StatusCode { get; } = statusCode;
+    public string? ErrorDescription { get; } = errorDescription;
+}
