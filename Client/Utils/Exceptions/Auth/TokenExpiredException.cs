f50a139a5df6dbb951d09c4f735858ea2f09de46
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 17:38:48 -0400

    add TokenExpiredException for handling expired authentication tokens

---
+++ Client/Utils/Exceptions/Auth/TokenExpiredException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Client.Utils.Enums;
+
+namespace Client.Utils.Exceptions.Auth;
+
+public class TokenExpiredException(
+    string message = "Authentication token has expired",
+    string userMessage = "Your session has expired. Please sign in again.",
+    AuthenticationTokenType tokenType = AuthenticationTokenType.AccessToken,
+    Exception? innerException = null)
+    : AuthenticationException(message, userMessage, tokenType, requiresRelogin: true, innerException)
+{
+    public DateTime ExpiredAt { get; init; } = DateTime.UtcNow;
+}
