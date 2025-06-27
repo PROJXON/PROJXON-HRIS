a50d15ed1cd34ca5acff41fa5a8681a7849b29c0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 16:52:54 -0400

    add ValidationException class for improved input validation handling

---
+++ Client/Utils/Exceptions/Auth/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Client.Utils.Enums;
+
+namespace Client.Utils.Exceptions.Auth;
+
+public class ValidationException(
+    string message,
+    string userMessage,
+    IReadOnlyDictionary<string, string[]> validationErrors,
+    string? fieldName,
+    Exception? innerException = null)
+    : ClientExceptionBase(message, userMessage, ErrorSeverity.Warning, ErrorCategory.Validation, shouldReport: false, isRetryable: false, suggestedActions:
+            ["Please check your input and try again"],
+        innerException)
+{
+    public IReadOnlyDictionary<string, string[]> ValidationErrors { get; } = validationErrors;
+    public string? FieldName { get; } = fieldName;
+}
