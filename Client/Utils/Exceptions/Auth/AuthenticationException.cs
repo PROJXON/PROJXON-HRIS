280dbd608e66b5582eb5dfb1d5455b31a5ceea9f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 16:35:52 -0400

    add AuthenticationException class for handling authentication errors

---
+++ Client/Utils/Exceptions/Auth/AuthenticationException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Client.Utils.Enums;
+
+namespace Client.Utils.Exceptions.Auth;
+
+public class AuthenticationException : ClientExceptionBase
+{
+    public AuthenticationTokenType TokenType { get; }
+    public bool RequiresRelogin { get; }
+
+    public AuthenticationException(
+        string message,
+        string userMessage = "Please sign in again to continue",
+        AuthenticationTokenType tokenType = AuthenticationTokenType.AccessToken,
+        bool requiresRelogin = true,
+        Exception? innerException = null
+    ) : base(
+        message,
+        userMessage,
+        ErrorSeverity.Warning,
+        ErrorCategory.Authentication,
+        shouldReport: true,
+        isRetryable: false,
+        suggestedActions: requiresRelogin ? new[] { "Sign in again" } : new[] { "Try again" },
+        innerException: innerException)
+    {
+        TokenType = tokenType;
+        RequiresRelogin = requiresRelogin;
+    }
+
+    protected override ClientExceptionBase CreateCopy(Dictionary<string, object>? context = null, string? correlationId = null)
+    {
+        return new AuthenticationException(
+            Message,
+            UserMessage,
+            TokenType,
+            RequiresRelogin,
+            InnerException);
+    }
+}
