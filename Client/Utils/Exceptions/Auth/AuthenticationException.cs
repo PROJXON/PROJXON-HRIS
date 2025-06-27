dabd4e310b77464c22c710529ea47de49d1d2126
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-27 16:39:38 -0400

    refactor AuthenticationException to use expression-bodied constructor and auto-properties

--- Client/Utils/Exceptions/Auth/AuthenticationException.cs
+++ Client/Utils/Exceptions/Auth/AuthenticationException.cs
@@ -4,19 +4,13 @@ using Client.Utils.Enums;
 
 namespace Client.Utils.Exceptions.Auth;
 
-public class AuthenticationException : ClientExceptionBase
-{
-    public AuthenticationTokenType TokenType { get; }
-    public bool RequiresRelogin { get; }
-
-    public AuthenticationException(
-        string message,
-        string userMessage = "Please sign in again to continue",
-        AuthenticationTokenType tokenType = AuthenticationTokenType.AccessToken,
-        bool requiresRelogin = true,
-        Exception? innerException = null
-    ) : base(
-        message,
+public class AuthenticationException(
+    string message,
+    string userMessage = "Please sign in again to continue",
+    AuthenticationTokenType tokenType = AuthenticationTokenType.AccessToken,
+    bool requiresRelogin = true,
+    Exception? innerException = null)
+    : ClientExceptionBase(message,
         userMessage,
         ErrorSeverity.Warning,
         ErrorCategory.Authentication,
@@ -24,10 +18,9 @@ public class AuthenticationException : ClientExceptionBase
         isRetryable: false,
         suggestedActions: requiresRelogin ? new[] { "Sign in again" } : new[] { "Try again" },
         innerException: innerException)
-    {
-        TokenType = tokenType;
-        RequiresRelogin = requiresRelogin;
-    }
+{
+    public AuthenticationTokenType TokenType { get; } = tokenType;
+    public bool RequiresRelogin { get; } = requiresRelogin;
 
     protected override ClientExceptionBase CreateCopy(Dictionary<string, object>? context = null, string? correlationId = null)
     {
