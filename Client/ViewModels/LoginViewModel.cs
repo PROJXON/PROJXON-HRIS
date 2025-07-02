b3ebe0764e6f048dba6dccbd8da6db4ce29355be
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-02 16:59:17 -0400

    add LoginViewModel for Google login functionality

---
+++ Client/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Client.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Client.ViewModels;
+
+public partial class LoginViewModel(IAuthenticationService authService)
+    : ViewModelBase
+{
+    [ObservableProperty]
+    private bool _isLoggingIn;
+
+    [RelayCommand]
+    private async Task LoginWithGoogleAsync()
+    {
+        IsLoggingIn = true;
+
+        try
+        {
+            await authService.LoginAsync();
+        }
+        finally
+        {
+            IsLoggingIn = false;
+        }
+    }
+}
