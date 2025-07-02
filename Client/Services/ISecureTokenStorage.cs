9b5843517e4154aa17550321750d0dc49ba61dd7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-01 21:08:32 -0400

    add ISecureTokenStorage interface for token management

---
+++ Client/Services/ISecureTokenStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Client.Services;
+
+public interface ISecureTokenStorage
+{
+    Task StoreTokenAsync(string key, string token);
+    Task<string?> GetTokenAsync(string key);
+    Task DeleteTokenAsync(string key);
+}
