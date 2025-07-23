e3cf920681982a504499603eddab41f2c78e0826
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-23 19:13:09 -0400

    add ViewModelFactory to facilitate ViewModel creation

---
+++ Client/Services/ViewModelFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Client.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client.Services;
+
+public class ViewModelFactory(IServiceProvider serviceProvider) : IViewModelFactory
+{
+    public T Create<T>() where T : ViewModelBase
+    {
+        return serviceProvider.GetRequiredService<T>();
+    }
+}
