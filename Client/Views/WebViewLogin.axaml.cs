d0b84b83045d96f00f5afa6984a392eccade04bf
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-28 12:04:26 -0400

    add WebViewLogin component for web-based login interface

---
+++ Client/Views/WebViewLogin.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.WebView;
+using Avalonia.Markup.Xaml;
+
+namespace Client.Views;
+
+public partial class WebViewLogin : Window
+{
+    public WebViewLogin()
+    {
+        InitializeComponent();
+    }
+}
