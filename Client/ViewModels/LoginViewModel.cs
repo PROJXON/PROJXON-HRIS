e9ac52faf98c7194936451d0789121dee5de4556
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-28 17:10:32 -0400

    add Google OAuth login dialog to LoginViewModel

--- Client/ViewModels/LoginViewModel.cs
+++ Client/ViewModels/LoginViewModel.cs
@@ -1,7 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 using Client.Services;
-using Client.Utils.Enums;
-using Client.Utils.Exceptions.Auth;
+using Client.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -17,6 +18,23 @@ public partial class LoginViewModel(IAuthenticationService authService)
     private async Task LoginWithGoogleAsync()
     {
         IsLoggingIn = true;
+        
+        var dialog = new NativeWebDialog
+        {
+            Title = "Google OAuth Login",
+            CanUserResize = true,
+            Source = new Uri("https://docs.avaloniaui.net/")
+        };
+
+        // Create TaskCompletionSource so we can wait until window is closed.
+        var tcs = new TaskCompletionSource();
+        dialog.Closing += (s, e) => tcs.SetResult();
+        
+        dialog.Resize(500, 500);
+        
+        dialog.Show();
+
+        await tcs.Task;
 
         try
         {
