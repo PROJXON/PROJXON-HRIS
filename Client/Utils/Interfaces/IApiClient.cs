6312886c5fd51ee4889d045052f81d2555d06452
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-08-11 12:07:52 -0400

    fix IApiClient PutAsync method to use generic type parameter

--- Client/Utils/Interfaces/IApiClient.cs
+++ Client/Utils/Interfaces/IApiClient.cs
@@ -9,6 +9,6 @@ public interface IApiClient
     Task<ApiResponse<T>> GetAllAsync<T>(string endpoint, CancellationToken cancellationToken = default);
     Task<ApiResponse<T>> GetByIdAsync<T>(string endpoint, int id, CancellationToken cancellationToken = default);
     Task<ApiResponse<T>> PostAsync<T>(string endpoint, object data, CancellationToken cancellationToken = default);
-    Task<ApiResponse<object?>> PutAsync<T>(string endpoint, int id, object data, CancellationToken cancellationToken = default);
+    Task<ApiResponse<T>> PutAsync<T>(string endpoint, int id, object data, CancellationToken cancellationToken = default);
     Task<ApiResponse<object?>> DeleteAsync<T>(string endpoint, CancellationToken cancellationToken = default);
 }
