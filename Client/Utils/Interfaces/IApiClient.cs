94d777e18b14606ce85fd77299b6ddaa70a21c0f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-08-11 11:54:59 -0400

    refactor IApiClient interface to include new async methods for CRUD operations with cancellation support

--- Client/Utils/Interfaces/IApiClient.cs
+++ Client/Utils/Interfaces/IApiClient.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Client.Utils.Classes;
 
@@ -5,5 +6,9 @@ namespace Client.Utils.Interfaces;
 
 public interface IApiClient
 {
-    Task<ApiResponse<T>> FetchAllAsync<T>();
+    Task<ApiResponse<T>> GetAllAsync<T>(string endpoint, CancellationToken cancellationToken = default);
+    Task<ApiResponse<T>> GetByIdAsync<T>(string endpoint, int id, CancellationToken cancellationToken = default);
+    Task<ApiResponse<object?>> PostAsync<T>(string endpoint, object data, CancellationToken cancellationToken = default);
+    Task<ApiResponse<object?>> PutAsync<T>(string endpoint, int id, object data, CancellationToken cancellationToken = default);
+    Task<ApiResponse<object?>> DeleteAsync<T>(string endpoint, CancellationToken cancellationToken = default);
 }
