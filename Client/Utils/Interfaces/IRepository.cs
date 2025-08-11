43d680c48ac49a4f2276d9ea945d6e4fe8c18ef5
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-08-11 12:19:07 -0400

    add asynchronous methods for CRUD operations in IRepository interface

--- Client/Utils/Interfaces/IRepository.cs
+++ Client/Utils/Interfaces/IRepository.cs
@@ -1,9 +1,15 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Client.Utils.Classes;
 
 namespace Client.Utils.Interfaces;
 
 public interface IRepository<TEntity, TKey> where TEntity : class
 {
     Task<IEnumerable<TEntity>> GetAllAsync<T>();
+    Task<Result<TEntity>> GetByIdAsync(TKey id, CancellationToken cancellationToken = default);
+    Task<Result<TEntity>> CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task<Result<TEntity>> UpdateAsync(TKey id, TEntity entity, CancellationToken cancellationToken = default);
+    Task<Result> DeleteAsync(TKey id, CancellationToken cancellationToken = default);
 }
