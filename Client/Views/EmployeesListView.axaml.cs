53331aa58b5d3bc511ded5a6a86693e7e28cec72
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-08-11 19:57:24 -0400

    load employees on view initialization

--- Client/Views/EmployeesListView.axaml.cs
+++ Client/Views/EmployeesListView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Client.ViewModels;
 
 namespace Client.Views;
 
@@ -9,5 +10,14 @@ public partial class EmployeesListView : UserControl
     public EmployeesListView()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private async void OnLoaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is EmployeesListViewModel viewModel)
+        {
+            await viewModel.LoadEmployeesCommand.ExecuteAsync(null);
+        }
     }
 }
