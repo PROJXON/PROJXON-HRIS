72382341029f54baef2d924963243b7000c95a47
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-08-16 19:15:00 -0400

    add EntityId property to NavigationEventArgs for enhanced navigation context

--- Client/Utils/Classes/NavigationEventArgs.cs
+++ Client/Utils/Classes/NavigationEventArgs.cs
@@ -3,7 +3,8 @@ using Client.Utils.Enums;
 
 namespace Client.Utils.Classes;
 
-public class NavigationEventArgs(ViewModelType viewModelType) : EventArgs
+public class NavigationEventArgs(ViewModelType viewModelType, int id = 0) : EventArgs
 {
     public ViewModelType ViewModelType { get; } = viewModelType;
+    public int EntityId { get; } = id;
 }
