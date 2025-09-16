95f0b53d565c2ba3eeee57a868b2e55acaf04ad2
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-09-16 03:16:23 -0400

    Edit EmployeeTraining
    Added fields to EmployeeTraining and EmployeeTrainingBase

--- CloudSync/Modules/EmployeeManagement/Models/EmployeeTraining.cs
+++ CloudSync/Modules/EmployeeManagement/Models/EmployeeTraining.cs
@@ -9,12 +9,30 @@ public class EmployeeTraining
     public int Id { get; set; }
     
     public Employee? Employee { get; set; }
+
+    [Column(TypeName = "jsonb")]
+    public List<string>? CanvasCertificates { get; set; }
     
     [Column(TypeName = "jsonb")]
     public List<string>? CanvasCoursesCompleted { get; set; }
-    
+
+    [StringLength(40)]
+    public string? DevelopmentPlan { get; set; }
+
+    public int? TrainingHoursLogged { get; set; }
+
     [Column(TypeName = "jsonb")]
-    public List<string>? CanvasCertificates { get; set; }
+    public List<string>? Evaluation { get; set; }
+
+    public bool? LeadershipParticipation { get; set; }
+
+    [Url]
+    [StringLength(200)]
+    public string? ParticipantDashboard { get; set; }
+
+    [Url]
+    [StringLength(200)]
+    public string? ParticipantMeetingLink { get; set; }
     
     [StringLength(40)]
     public string? NewCompany { get; set; }
