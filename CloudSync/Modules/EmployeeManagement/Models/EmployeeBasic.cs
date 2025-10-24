1eceedbbb1d85ed8695d952485e48c11a91f654c
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-10-24 04:34:06 -0400

    Updated EmployeeBasic model, BaseDto, UpdateEmployeeRequest, and added migration UpdatedEmployeeBasicFields

--- CloudSync/Modules/EmployeeManagement/Models/EmployeeBasic.cs
+++ CloudSync/Modules/EmployeeManagement/Models/EmployeeBasic.cs
@@ -5,41 +5,15 @@ namespace CloudSync.Modules.EmployeeManagement.Models;
 
 public class EmployeeBasic
 {
-    [StringLength(40)]
-    public string? FirstName { get; set; }
-    
-    [StringLength(40)]
-    public string? LastName { get; set; }
-    
-    [StringLength(40)] 
-    public string? Nationality { get; set; }
-
-    [StringLength(40)]
-    public string? Ethnicity { get; set; }
-    
-    [StringLength(40)]
-    public string? BirthCountry { get; set; }
-    
-    [StringLength(40)]
-    public string? BirthState { get; set; }
-    
-    [StringLength(40)]
-    public string? BirthCity { get; set; }
-    
-    public DateTime? BirthDate { get; set; }
-    
-    [StringLength(40)]
-    public string? Gender { get; set; }
-    
-    [StringLength(20)]
-    public string? MaritalStatus { get; set; }
-    
-    [StringLength(40)]
-    public string? PreferredPronouns { get; set; }
-    
-    [StringLength(40)]
-    public string? PreferredName { get; set; }
-    
-    [StringLength(40)]
-    public string? NickName { get; set; }
+    [StringLength(40)] public string? FirstName { get; set; }
+    [StringLength(40)] public string? LastName { get; set; }
+    [StringLength(40)] public string? PreferredName { get; set; }
+    [StringLength(40)] public string? NamePronunciation { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    [StringLength(40)] public string? Gender { get; set; }
+    [StringLength(20)] public string? MaritalStatus { get; set; }
+    [StringLength(40)] public string? Nationality { get; set; }
+    [StringLength(40)] public string? Race { get; set; }
+    [StringLength(40)] public string? Ethnicity { get; set; }
+    [StringLength(40)] public string? PreferredPronouns { get; set; }
 }
