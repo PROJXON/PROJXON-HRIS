621867d431cf36f2ea1da3137afee1caa04e957b
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-10-24 05:17:10 -0400

    Synced BaseDtos, UpdateEmployeeRequest, and docker-compose changes

--- Shared/EmployeeManagement/BaseDtos/EmployeeBasicBase.cs
+++ Shared/EmployeeManagement/BaseDtos/EmployeeBasicBase.cs
@@ -4,15 +4,13 @@ public class EmployeeBasicBase
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Nationality { get; set; }
-    public string? Ethnicity { get; set; }
-    public string? BirthCountry { get; set; }
-    public string? BirthState { get; set; }
-    public string? BirthCity { get; set; }
-    public DateTime? BirthDate { get; set; }
+    public string? PreferredName { get; set; }
+    public string? NamePronunciation { get; set; }
+    public DateTime? DateOfBirth { get; set; }
     public string? Gender { get; set; }
     public string? MaritalStatus { get; set; }
+    public string? Nationality { get; set; }
+    public string? Race { get; set; }
+    public string? Ethnicity { get; set; }
     public string? PreferredPronouns { get; set; }
-    public string? PreferredName { get; set; }
-    public string? NickName { get; set; }
 }
