640b686fd0354528d3d460c6e8d102bcb3805073
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-10-29 06:34:23 -0400

    Updated EmployeePosition model and added migration UpdatedEmployeePositionFields

--- Shared/EmployeeManagement/BaseDtos/EmployeePositionBase.cs
+++ Shared/EmployeeManagement/BaseDtos/EmployeePositionBase.cs
@@ -5,17 +5,22 @@ namespace Shared.EmployeeManagement.BaseDtos;
 public class EmployeePositionBase
 {
     public int Id { get; set; }
-    public string? PositionName { get; set; }
-    public int? DepartmentId { get; set; }
-    public int? SubDepartmentId { get; set; }
-    public string? HierarchyLevel { get; set; }
-    public ManagerOrCoachSummary? Manager { get; set; }
-    public ManagerOrCoachSummary? Coach { get; set; }
+    public string? ProjxonEmail { get; set; }
     public DateTime? OnboardingDate { get; set; }
     public DateTime? OffboardingDate { get; set; }
-    public DateTime? HireDate { get; set; }
-    public DateTime? ExitDate { get; set; }
     public string? EmploymentStatus { get; set; }
     public string? EmploymentType { get; set; }
+    public string? EmployeeLifeCycleStage { get; set; }
+    public string? PositionName { get; set; }
+    public int? ManagerId { get; set; }
+    public ManagerOrCoachSummary? Manager { get; set; }
+    public int? DepartmentId { get; set; }
+    public int? SubDepartmentId { get; set; }
+    public int? CoachId { get; set; }
+    public ManagerOrCoachSummary? Coach { get; set; }
+    public string? PreviousEmployers { get; set; }
+    public string? HierarchyLevel { get; set; }
+    public DateTime? HireDate { get; set; }
+    public DateTime? ExitDate { get; set; }
     public string? RecruitingSource { get; set; }
 }
