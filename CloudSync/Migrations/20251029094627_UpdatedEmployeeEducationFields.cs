12309bc5691afb60497982cc110b455ea4b90def
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-10-29 05:50:37 -0400

    Updated EmployeeEducation model and added migration UpdatedEmployeeEducationFields

---
+++ CloudSync/Migrations/20251029094627_UpdatedEmployeeEducationFields.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedEmployeeEducationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "graduate_degree",
+                table: "employee_educations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "graduate_school",
+                table: "employee_educations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "undergraduate_degree",
+                table: "employee_educations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "undergraduate_school",
+                table: "employee_educations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "graduate_degree",
+                table: "employee_educations");
+
+            migrationBuilder.DropColumn(
+                name: "graduate_school",
+                table: "employee_educations");
+
+            migrationBuilder.DropColumn(
+                name: "undergraduate_degree",
+                table: "employee_educations");
+
+            migrationBuilder.DropColumn(
+                name: "undergraduate_school",
+                table: "employee_educations");
+        }
+    }
+}
