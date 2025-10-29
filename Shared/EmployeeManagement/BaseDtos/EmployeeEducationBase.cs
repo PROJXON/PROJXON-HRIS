86abdf05b2e193b62991b34b69fee9c4d2bd5242
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-10-29 05:53:58 -0400

    Updated EmployeeEducationBase DTO to match new EmployeeEducation model

--- Shared/EmployeeManagement/BaseDtos/EmployeeEducationBase.cs
+++ Shared/EmployeeManagement/BaseDtos/EmployeeEducationBase.cs
@@ -4,6 +4,10 @@ public class EmployeeEducationBase
 {
     public int Id { get; set; }
     public string? EducationLevel { get; set; }
+    public string? UndergraduateSchool { get; set; }
+    public string? UndergraduateDegree { get; set; }
+    public string? GraduateSchool { get; set; }
+    public string? GraduateDegree { get; set; }
     public List<string>? UniversitiesAttended { get; set; }
     public List<string>? DegreesEarned { get; set; }
 }
