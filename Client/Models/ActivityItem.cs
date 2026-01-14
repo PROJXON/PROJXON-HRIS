77a7284f6e4508901b608ee38bb12da5a49ec09f
toast212 <156926783+toast212@users.noreply.github.com>
2026-01-14 16:21:33 -0500

    bugfix: remove duplicate ActivityItem.cs

--- Client/Models/ActivityItem.cs
+++
@@ -1,8 +0,0 @@
-namespace Client.Models;
-
-public class ActivityItem
-{
-    public string Title { get; set; } = string.Empty;
-    public string Subtitle { get; set; } = string.Empty;
-    public string TimeAgo { get; set; } = string.Empty;
-}
